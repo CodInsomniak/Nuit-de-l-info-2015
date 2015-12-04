e0b4a06f912e25e27edee51befc59cc02eb58228
Simon Prévidente <simon.previdente@free.fr>
2015-12-04 04:47:38 +0100

    Draggable marker from ionic menu

--- www/js/app.js
+++ www/js/app.js
@@ -8,15 +8,11 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
@@ -51,6 +47,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   })
 
-  // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/map');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,42 @@
 angular.module('starter.controllers', [])
 
 .controller('MapCtrl', function($scope) {
+
+  // Leaflet
   var map = L.map('map').setView([51.505, -0.09], 13);
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
+
+  // Marker
+  var markers = [], // an array containing all the markers added to the map
+      markersCount = 0; // the number of the added markers
+  var posTop = $( '.draggable-marker' ).css( 'top' ),
+      posLeft = $( '.draggable-marker' ).css( 'left' );
+  $( '.draggable-marker' ).draggable({
+    stop: function ( event, ui ) {
+      $( '.draggable-marker' ).css( 'top', posTop );
+      $( '.draggable-marker' ).css( 'left', posLeft );
+      var coordsX = event.clientX - 275, // 50 is the width of the menu
+          coordsY = event.clientY - 40, // 20 is the half of markers height
+          point = L.point( coordsX, coordsY ), // createing a Point object with the given x and y coordinates
+          markerCoords = map.containerPointToLatLng( point ), // getting the geographical coordinates of the point
+      // Creating a custom icon
+      myIcon = L.icon({
+        iconUrl: 'img/marker-icon.png', // the url of the img
+        iconSize: [20, 40],
+        iconAnchor: [10, 40] // the coordinates of the "tip" of the icon ( in this case must be ( icon width/ 2, icon height )
+      });
+      // Creating a new marker and adding it to the map
+      markers[ markersCount ] = L.marker( [ markerCoords.lat, markerCoords.lng ], {
+        draggable: true,
+        icon: myIcon
+      }).addTo( map );
+
+      markersCount++;
+    }
+  });
+
 })
 
 .controller('ConfigCtrl', function(){
